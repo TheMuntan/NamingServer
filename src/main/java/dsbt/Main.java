07d60076482f1c7343f0496b830cb74056cc7a9e
TheMuntan <serdar_tst@hotmail.com>
2021-04-22 10:02:09 +0200

    added base rest server

--- src/main/java/dsbt/Main.java
+++ src/main/java/dsbt/Main.java
@@ -1,9 +1,21 @@
 package dsbt;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Name this server plz");
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+	public static void main(String[] args) {
+	SpringApplication.run(Main.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+	}
+
 }

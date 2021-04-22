ffff3b154dcca7c044af8622e5ca639d7228ab4e
VladGoguta <gogutavlad@gmail.com>
2021-04-22 10:38:17 +0100

    MapDatabase

---
+++ src/main/java/dsbt/MapDatabase.java
@@ -0,0 +1,20 @@
+package dsbt;
+
+import java.util.HashMap;
+
+public class MapDatabase {
+
+    private static MapDatabase singleToneMap = null;
+    private HashMap<Integer,String> map;
+
+
+    public MapDatabase() {
+        HashMap<Integer,String> map = new HashMap<>();
+    }
+
+    public static  MapDatabase getInstanceMap(){
+        if (singleToneMap == null)
+            singleToneMap = new MapDatabase();
+        return singleToneMap;
+    }
+}

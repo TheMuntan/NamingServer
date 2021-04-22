2ff96ac9209ceaa706b546bbfce9aa8cb884a1d7
TheMuntan <serdar_tst@hotmail.com>
2021-04-22 10:05:53 +0200

    maven fix

--- src/main/java/dsbt/NamingController.java
+++ src/main/java/dsbt/NamingController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package dsbt;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class NamingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();

c9840bf3c81cfdfcf42c0a2a3bd66325dd294ce8
Martino <martino.daboomster@telenet.be>
2021-04-26 11:08:54 +0200

    Fixed HashFunction and added tests

---
+++ src/test/java/dsbt/HashFunctionTest.java
@@ -0,0 +1,13 @@
+package dsbt;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HashFunctionTest {
+    @Test
+    public void HashTest() {
+        Assert.assertEquals(163840, HashFunction.getIntHash(0));
+        Assert.assertEquals(0, HashFunction.getIntHash(Integer.MIN_VALUE));
+        Assert.assertEquals(327680, HashFunction.getIntHash(Integer.MAX_VALUE));
+    }
+}
